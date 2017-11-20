7433e388b7eda69728e07cb526feb9953e993344
🦊Tomoya Hirano <noppelabs@gmail.com>
2017-11-20 14:28:03 +0900

    support carthage

---
+++ Carthage/NFRxSwiftExt/NFRxSwiftExt/NFRxSwiftExt.h
@@ -0,0 +1,19 @@
+//
+//  NFRxSwiftExt.h
+//  NFRxSwiftExt
+//
+//  Created by Tomoya Hirano on 2017/11/20.
+//  Copyright © 2017年 Tomoya Hirano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NFRxSwiftExt.
+FOUNDATION_EXPORT double NFRxSwiftExtVersionNumber;
+
+//! Project version string for NFRxSwiftExt.
+FOUNDATION_EXPORT const unsigned char NFRxSwiftExtVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NFRxSwiftExt/PublicHeader.h>
+
+
